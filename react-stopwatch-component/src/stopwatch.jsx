bdc7c76b13ee74c74c85f4211958b56449afa00b
Semi Lee <semilee6557@gmail.com>
2021-03-25 04:17:19 +0000

    trying to add table when click split btn

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isPlay: false, time: { mins: 0, sec: 0, milisec: 0 } };
+    this.state = { isPlay: false, time: { mins: 0, sec: 0, milisec: 0 }, isLapClicked: false, lap: [] };
     this.handleClick = this.handleClick.bind(this);
     this.changeController = this.changeController.bind(this);
     this.timerOn = this.timerOn.bind(this);
     this.timerOff = this.timerOff.bind(this);
     this.handleTimer = this.handleTimer.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    this.handleLap = this.handleLap.bind(this);
+    this.createTable = this.createTable.bind(this);
+    this.unClickedLapBtn = this.unClickedLapBtn.bind(this);
   }
 
   handleClick() {
@@ -71,17 +74,48 @@ class Stopwatch extends React.Component {
     return { mins: mins, sec: sec, milisec: milisec };
   }
 
+  handleLap() {
+    const split = this.displayTime();
+    const data = this.state.lap;
+    data.push(split);
+    this.setState({ lap: data }, { isLapClicked: true });
+  }
+
+  unClickedLapBtn() {
+    this.setState({ isLapClicked: false });
+  }
+
+  createTable() {
+    const data = this.state.lap;
+    let htmlTag = '';
+    for (let i = 0; i < data.length; i++) {
+      htmlTag += '<tr>< th scope = "row" >' + i + 1 + '</th><td>' + data[i] + '</td><td>Split</td></tr >';
+    }
+    return htmlTag;
+  }
+
   render() {
     const controller = this.changeController();
     const { mins, sec, milisec } = this.displayTime();
+    let tag = '';
+    if (this.state.isLapClicked) {
+      tag = this.createTable();
+      this.unClickedLapBtn();
+    }
 
     return (
       <div className="container">
         <button onClick={this.handleReset} className="stopWatch"> {mins}:{sec}:{milisec} </button>
         <button onClick={this.handleClick} className="control">{controller}</button>
+        <button onClick={this.handleLap}>Split</button>
+        <table className="table">{tag}</table>
       </div>
     );
   }
+}
+
+function table(props) {
+  return <table></table>;
 }
 
 export default Stopwatch;
