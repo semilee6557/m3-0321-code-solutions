989c2b71969c9e0dbb44d14f3abce92c20f94577
Semi Lee <semilee6557@gmail.com>
2021-03-26 02:45:24 +0000

    Complete exercise

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function PokemonList(props) {
+  const pokedex = props.pokedex;
+  const listPokedex = pokedex.map(pokedex =>
+    <li key={pokedex.name}>{pokedex.name}</li>
+  );
+  return (
+    <ul>{listPokedex}</ul>
+  );
+
+}
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+ReactDOM.render(
+    <PokemonList pokedex={pokedex} />,
+    document.querySelector('#root')
+);
