e1a6a66c1e504e2aab61621552beb5610d1e7c7f
Semi Lee <semilee6557@gmail.com>
2021-03-26 04:17:29 +0000

    error on table

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isPlay: false, time: { mins: 0, sec: 0, milisec: 0 }, isLapClicked: false, lap: [] };
+    this.state = { isPlay: false, time: { mins: 0, sec: 0, milisec: 0 }, lap: [], nextLapId: 1 };
     this.handleClick = this.handleClick.bind(this);
     this.changeController = this.changeController.bind(this);
     this.timerOn = this.timerOn.bind(this);
@@ -11,7 +11,6 @@ class Stopwatch extends React.Component {
     this.handleTimer = this.handleTimer.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.handleLap = this.handleLap.bind(this);
-    this.unClickedLapBtn = this.unClickedLapBtn.bind(this);
   }
 
   handleClick() {
@@ -30,6 +29,8 @@ class Stopwatch extends React.Component {
   handleReset() {
     if (!this.state.isPlay) {
       this.setState({ time: { mins: 0, sec: 0, milisec: 0 } });
+      this.setState({ lap: [] });
+      this.setState({ nextLapId: 1 });
     }
   }
 
@@ -76,42 +77,45 @@ class Stopwatch extends React.Component {
   handleLap() {
     const split = this.displayTime();
     const data = this.state.lap;
-    data.push(split);
+    data.push({ id: this.state.nextLapId, time: split });
     this.setState({ lap: data });
-    this.setState({ isLapClicked: true });
-  }
-
-  unClickedLapBtn() {
-    this.setState({ isLapClicked: false });
+    let nextLapId = this.state.nextLapId;
+    nextLapId++;
+    this.setState({ nextLapId: nextLapId });
+    // console.log(this.state);
   }
 
   render() {
     const controller = this.changeController();
     const { mins, sec, milisec } = this.displayTime();
-    let tag = '';
-    if (this.state.isLapClicked) {
-      tag = <table data={this.state} />;
-      this.unClickedLapBtn();
-      // console.log('tag', tag);
-    }
+
     return (
       <div className="container">
         <button onClick={this.handleReset} className="stopWatch"> {mins}:{sec}:{milisec} </button>
         <button onClick={this.handleClick} className="control">{controller}</button>
         <button onClick={this.handleLap}>Split</button>
-        <table className="table">{tag}</table>
+        <Table lap={this.state.lap} />
       </div>
     );
   }
 }
 
-function table(prop) {
-  const data = prop.lap;
-  let htmlTag = '';
-  for (let i = 0; i < data.length; i++) {
-    htmlTag += '<tr><th scope = "row" >' + i + 1 + '</th><td>' + data[i] + '</td><td>Split</td></tr>';
-  }
-  return htmlTag;
+function Table(props) {
+  const lap = props.lap;
+  const lapList = lap.map(lap =>
+    <tbody key={lap.id}>
+     <tr>
+      <th scope="row" >{lap.id}</th>
+      <td>{lap.time}</td>
+      <td>Split</td>
+     </tr>
+    </tbody>
+  );
+  return (
+    <table className="table">
+      {lapList}
+    </table>
+  );
 }
 
 export default Stopwatch;
