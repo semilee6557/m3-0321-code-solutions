89376445db227fbf1f9b76339e5b5030e885ba8c
Semi Lee <semilee6557@gmail.com>
2021-03-31 04:53:27 +0000

    Complete exercise

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,79 +1,60 @@
 import React from 'react';
 
+class Title extends React.Component {
+  render() {
+    const title = this.props.title;
+    const id = this.props.id;
+    return (
+      <tr className="title">
+        <td id={id}>{title}</td>
+      </tr>
+    );
+  }
+}
+
+class Content extends React.Component {
+  render() {
+    const content = this.props.content;
+    return (
+        <tr>
+          <td>{content}</td>
+        </tr>
+    );
+  }
+}
+
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { clicked: '' };
+    this.state = { clickedId: '' };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(event) {
-    const className = event.target.className;
-    if (className === 'html' || className === 'css' || className === 'js') {
-      if (this.state.clicked !== className) {
-        this.setState({ clicked: event.target.className });
-      } else {
-        this.setState({ clicked: '' });
-      }
+    const id = event.target.id;
+    if (this.state.clickedId === id) {
+      this.setState({ clickedId: '' });
+    } else {
+      this.setState({ clickedId: id });
     }
   }
 
   render() {
-
-    let html = 'hide';
-    let css = 'hide';
-    let js = 'hide';
-
-    if (this.state.clicked === 'html') {
-      html = '';
-    } else if (this.state.clicked === 'css') {
-      css = '';
-    } else if (this.state.clicked === 'js') {
-      js = '';
-    }
+    const rows = [];
+    this.props.topics.forEach(topic => {
+      rows.push(<Title id={topic.id} title={topic.title} />);
+      if (this.state.clickedId === topic.id) {
+        rows.push(<Content content={topic.content} />);
+      }
+    });
 
     return (
-      <table className="table table-striped table-bordered" onClick={this.handleClick}>
-        <tbody>
-          <tr>
-            <td className="html">
-              Hypertext Markup Language
-          </td>
-          </tr>
-          <tr>
-
-            <td className={html}>Hypertext Markup Language (HTML) is the standard markup language for creating web pages and
-            web
-            applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies
-            for the World Wide Web.</td>
-          </tr>
-          <tr>
-
-            <td className="css">Cascading Style Sheets</td>
-          </tr>
-          <tr>
-
-            <td className={css}>Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation
-            of a document
-            written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web. alon a side
-            HTML and JAvaScript.</td>
-          </tr>
-          <tr>
-
-            <td className="js">JavaScript</td>
-          </tr>
-          <tr>
-
-            <td className={js}>Javascript, often abbreviated as JS, is a high-level, interpreted programming language that
-            conforms to
-            the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based
-            object-orientation. and first-class functions.</td>
-          </tr>
-
+      <table className="table table-bordered" >
+        <tbody onClick={this.handleClick}>
+         {rows}
         </tbody>
       </table>
-
     );
   }
 }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './accordion';
 
+const TOPICS = [
+  {
+    id: 'html',
+    title: 'Hypertext Markup Language',
+    content: 'Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications.With Cascading Style Sheets(CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.'
+  },
+  {
+    id: 'css',
+    title: 'Cascading Style Sheets',
+    content: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML.CSS is a cornerstone technology of the World Wide Web.alon a side HTML and JAvaScript.'
+  },
+  {
+    id: 'js',
+    title: 'JavaScript',
+    content: 'Javascript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification.JavaScript has curly- bracket syntax, dynamic typing, prototype - based object - orientation.and first - class functions.'
+  }
+];
+
 ReactDOM.render(
-  <Accordion />,
+  <Accordion topics={TOPICS}/>,
   document.querySelector('#root')
 );
